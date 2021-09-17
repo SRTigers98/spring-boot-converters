8d2b19c2559f08acf973c4a99bbbe6e7b960d37e
Eder Benjamin <ben-eder@freenet.de>
2021-09-17 15:30:53 +0200

    Get MockMvc via application context to run the tests with gradle Additional the line separators of the test files were replaced like in the unit tests

--- spring-boot-converters-itest/src/test/java/com/github/srtigers98/spring/boot/converters/itest/web/XmlControllerTest.java
+++ spring-boot-converters-itest/src/test/java/com/github/srtigers98/spring/boot/converters/itest/web/XmlControllerTest.java
@@ -2,17 +2,17 @@ package com.github.srtigers98.spring.boot.converters.itest.web;
 
 import com.github.srtigers98.spring.boot.converters.itest.dao.XmlDocumentRepository;
 import com.github.srtigers98.spring.boot.converters.itest.entity.XmlDocument;
+import com.github.srtigers98.spring.boot.converters.itest.util.IntegrationTest;
 import org.assertj.core.util.Files;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
 
@@ -21,14 +21,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @ActiveProfiles("xml")
-class XmlControllerTest {
+class XmlControllerTest extends IntegrationTest {
 
   @Autowired
-  private MockMvc               mvc;
-  @Autowired
   private XmlDocumentRepository repository;
+
+  @Autowired
+  XmlControllerTest(WebApplicationContext webApplicationContext) {
+    super(webApplicationContext);
+  }
 
   @BeforeEach
   void setUp() {
@@ -37,31 +39,33 @@ class XmlControllerTest {
 
   @Test
   void getXmlDocumentTest() throws Exception {
-    var testGetXml = Files.contentOf(ResourceUtils.getFile("classpath:xml/test-get.xml"),
-                                     StandardCharsets.UTF_8);
+    var xml = Files.contentOf(ResourceUtils.getFile("classpath:xml/test-get.xml"),
+                              StandardCharsets.UTF_8);
+    var testGetXml = this.sanitizeFileContent(xml);
 
     var request = MockMvcRequestBuilders.get("/xml")
                                         .queryParam("id", testDocumentGet().getId().toString());
 
-    mvc.perform(request)
-       .andExpect(status().isOk())
-       .andExpect(mvcResult -> {
-         var result = mvcResult.getResponse().getContentAsString();
-         assertThat(result, is(testGetXml));
-       });
+    this.mvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(mvcResult -> {
+              var result = mvcResult.getResponse().getContentAsString();
+              assertThat(result, is(testGetXml));
+            });
   }
 
   @Test
   void saveXmlDocumentTest() throws Exception {
     var xml = Files.contentOf(ResourceUtils.getFile("classpath:xml/test-post.xml"),
                               StandardCharsets.UTF_8);
+    var testPostXml = this.sanitizeFileContent(xml);
 
     var request = MockMvcRequestBuilders.post("/xml")
                                         .contentType(MediaType.APPLICATION_XML)
-                                        .content(xml);
+                                        .content(testPostXml);
 
-    mvc.perform(request)
-       .andExpect(status().isOk());
+    this.mvc.perform(request)
+            .andExpect(status().isOk());
 
     assertThat(repository.findById(99).isPresent(), is(true));
     assertThat(repository.findById(99).get(), is(testDocumentPost()));
