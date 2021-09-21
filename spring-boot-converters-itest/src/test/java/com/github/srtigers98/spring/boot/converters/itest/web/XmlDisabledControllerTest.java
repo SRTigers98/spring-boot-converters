0aa074772d900267cf5ca9c51c8cb87e63f2db6e
Eder Benjamin <ben-eder@freenet.de>
2021-09-21 20:00:14 +0200

    Integration test to check cases when enable-xml is false

---
+++ spring-boot-converters-itest/src/test/java/com/github/srtigers98/spring/boot/converters/itest/web/XmlDisabledControllerTest.java
@@ -0,0 +1,57 @@
+package com.github.srtigers98.spring.boot.converters.itest.web;
+
+import com.github.srtigers98.spring.boot.converters.itest.dao.XmlDocumentRepository;
+import com.github.srtigers98.spring.boot.converters.itest.entity.XmlDocument;
+import com.github.srtigers98.spring.boot.converters.itest.util.IntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.file.Files;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class XmlDisabledControllerTest extends IntegrationTest {
+
+  private final XmlDocumentRepository repository;
+
+  @Autowired
+  protected XmlDisabledControllerTest(WebApplicationContext webApplicationContext,
+                                      XmlDocumentRepository repository) {
+    super(webApplicationContext);
+    this.repository = repository;
+  }
+
+  @BeforeEach
+  void setUp() {
+    this.repository.save(new XmlDocument(0, "A random message"));
+  }
+
+  @Test
+  void getXmlDocumentTest() throws Exception {
+    var request = MockMvcRequestBuilders.get("/xml")
+                                        .queryParam("id", "0");
+
+    this.mvc.perform(request)
+            .andExpect(status().isInternalServerError());
+  }
+
+  @Test
+  void saveXmlDocumentTest() throws Exception {
+    var xml = Files.readString(ResourceUtils.getFile("classpath:xml/test-post.xml")
+                                            .toPath());
+
+    var request = MockMvcRequestBuilders.post("/xml")
+                                        .contentType(MediaType.APPLICATION_XML)
+                                        .content(xml);
+
+    this.mvc.perform(request)
+            .andExpect(status().isUnsupportedMediaType());
+  }
+}
